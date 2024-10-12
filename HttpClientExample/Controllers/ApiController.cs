b213358f961a855bc8b94bed41f2a39459f17b57
Marija Topalova <marijatopalova1@outlook.com>
2024-10-12 15:10:23 +0200

    Add project files.

---
+++ HttpClientExample/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using HttpClientExample.Models;
+using HttpClientExample.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController(ApiService apiService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Post>>> GetAllPosts()
+        {
+            var result = await apiService.GetDataAsync();
+
+            return Ok(result);
+        }
+    }
+}
